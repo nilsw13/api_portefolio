4cff3fd2d969e7ec50a9afb0965f7e619e97f2c3
nilsw13 <nils.wenting@laplateforme.io>
2025-02-06 15:26:47 +0100

    change comande line runner

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS projects;
+
+CREATE TABLE projects (
+                          id SERIAL PRIMARY KEY,
+                          project_name VARCHAR(255),
+                          project_description TEXT,
+                          backend_stack VARCHAR(255),
+                          frontend_stack VARCHAR(255),
+                          database_stack VARCHAR(255),
+                          backend_deployment_stack VARCHAR(255),
+                          frontend_deployment_stack VARCHAR(255),
+                          project_link VARCHAR(255),
+                          image_url_1 VARCHAR(255),
+                          image_url_2 VARCHAR(255),
+                          visible BOOLEAN
+);
