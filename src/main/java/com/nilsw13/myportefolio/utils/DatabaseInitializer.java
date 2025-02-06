cb307c58ec2416b995e2fcb336f932397a0fd903
nilsw13 <nils.wenting@laplateforme.io>
2025-02-06 05:22:07 +0100

    set up postgres config

--- src/main/java/com/nilsw13/myportefolio/utils/DatabaseInitializer.java
+++ src/main/java/com/nilsw13/myportefolio/utils/DatabaseInitializer.java
@@ -4,6 +4,7 @@ import com.nilsw13.myportefolio.models.Messages;
 import com.nilsw13.myportefolio.models.Projects;
 import com.nilsw13.myportefolio.repositories.MessageRepository;
 import com.nilsw13.myportefolio.repositories.ProjectRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,11 @@ import org.springframework.stereotype.Component;
 public class DatabaseInitializer implements CommandLineRunner {
 
     private final ProjectRepository projectRepository;
+    private EntityManager entityManager;
 
-    public DatabaseInitializer(ProjectRepository projectRepository) {
+    public DatabaseInitializer(ProjectRepository projectRepository, EntityManager entityManager) {
         this.projectRepository = projectRepository;
+        this.entityManager = entityManager;
     }
 
 
@@ -22,75 +25,86 @@ public class DatabaseInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         // verify if the database is empty
+        //
+        //
+        //
 
-        if (projectRepository.count() > 0) {
-            System.out.println("Database already initialized");
-            return ;
-        }else {
-            Projects project1 = new Projects();
+        try {
+            waitForDatabase();
 
-            project1.setProjectName("Studio Headshot Project");
-            project1.setProjectDescription("A SaaS platform that allows people to generate professional headshot photos with AI.Makes them save time and money.");
-            project1.setBackendStack("Java - Spring Boot");
-            project1.setFrontendStack("ReactJs - Vite");
-            project1.setDatabaseStack("PostgreSQL");
-            project1.setBackendDeploymentStack("Heroku");
-            project1.setFrontendDeploymentStack("Netlify");
-            project1.setProjectLink("https://studioheadshotpro.com");
-            project1.setImageUrl1("/projectIllustration/studioheadshotpro1.png");
-            project1.setImageUrl2("/projectIllustration/studioheadshotpro2.png");
-            project1.setVisible(true);
+            Long count = projectRepository.count();
 
-            projectRepository.save(project1);
+            if (count == 0) {
+                Projects project1 = new Projects();
 
+                project1.setProjectName("Studio Headshot Project");
+                project1.setProjectDescription("A SaaS platform that allows people to generate professional headshot photos with AI.Makes them save time and money.");
+                project1.setBackendStack("Java - Spring Boot");
+                project1.setFrontendStack("ReactJs - Vite");
+                project1.setDatabaseStack("PostgreSQL");
+                project1.setBackendDeploymentStack("Heroku");
+                project1.setFrontendDeploymentStack("Netlify");
+                project1.setProjectLink("https://studioheadshotpro.com");
+                project1.setImageUrl1("/projectIllustration/studioheadshotpro1.png");
+                project1.setImageUrl2("/projectIllustration/studioheadshotpro2.png");
+                project1.setVisible(true);
 
-            Projects project2 = new Projects();
-            project2.setProjectName("KM CHANGE");
-            project2.setProjectDescription("A Money exchange shop and metals investment.");
-            project2.setBackendStack("Java - Spring Boot");
-            project2.setFrontendStack("thymeleaf - htmx");
-            project2.setDatabaseStack("PostgreSQL");
-            project2.setBackendDeploymentStack("Heroku");
-            project2.setFrontendDeploymentStack("Heroku");
-            project2.setProjectLink("https://www.kmchange.fr/");
-            project2.setImageUrl1("/projectIllustration/kmchange1.png");
-            project2.setImageUrl2("/projectIllustration/kmchange2.png");
-            project2.setVisible(true);
-
-            projectRepository.save(project2);
+                projectRepository.save(project1);
 
 
-            Projects project3 = new Projects();
-            project3.setProjectName("Java Top Tools");
-            project3.setProjectDescription("A Spring Boot REST API that fetches and serves information about the most popular Java repositories on GitHub");
-            project3.setBackendStack("Java");
-            project3.setFrontendStack("ReactJs - Vite");
-            project3.setDatabaseStack("None");
-            project3.setBackendDeploymentStack("Railway");
-            project3.setFrontendDeploymentStack("Vercel");
-            project3.setProjectLink("https://java-top-tools-front.vercel.app/");
-            project3.setImageUrl1("/projectIllustration/javatoptools1.png");
-            project3.setImageUrl2("/projectIllustration/javatoptools2.png");
-            project3.setVisible(true);
+                Projects project2 = new Projects();
+                project2.setProjectName("KM CHANGE");
+                project2.setProjectDescription("A Money exchange shop and metals investment.");
+                project2.setBackendStack("Java - Spring Boot");
+                project2.setFrontendStack("thymeleaf - htmx");
+                project2.setDatabaseStack("PostgreSQL");
+                project2.setBackendDeploymentStack("Heroku");
+                project2.setFrontendDeploymentStack("Heroku");
+                project2.setProjectLink("https://www.kmchange.fr/");
+                project2.setImageUrl1("/projectIllustration/kmchange1.png");
+                project2.setImageUrl2("/projectIllustration/kmchange2.png");
+                project2.setVisible(true);
+
+                projectRepository.save(project2);
 
 
-            projectRepository.save(project3);
+                Projects project3 = new Projects();
+                project3.setProjectName("Java Top Tools");
+                project3.setProjectDescription("A Spring Boot REST API that fetches and serves information about the most popular Java repositories on GitHub");
+                project3.setBackendStack("Java");
+                project3.setFrontendStack("ReactJs - Vite");
+                project3.setDatabaseStack("None");
+                project3.setBackendDeploymentStack("Railway");
+                project3.setFrontendDeploymentStack("Vercel");
+                project3.setProjectLink("https://java-top-tools-front.vercel.app/");
+                project3.setImageUrl1("/projectIllustration/javatoptools1.png");
+                project3.setImageUrl2("/projectIllustration/javatoptools2.png");
+                project3.setVisible(true);
 
 
-            Projects project4 = new Projects();
-            project4.setProjectName("Portfolio");
-            project4.setProjectDescription("A portfolio website that showcases my projects and skills.");
-            project4.setBackendStack("Java - Spring Boot");
-            project4.setFrontendStack("ReactJs - Vite");
-            project4.setDatabaseStack("SQLite");
-            project4.setBackendDeploymentStack("Heroku");
-            project4.setFrontendDeploymentStack("Netlify");
-            project4.setProjectLink("https://nilswenting.com/");
-            project4.setImageUrl1("/projectIllustration/portfolio1.png");
-            project4.setImageUrl2("/projectIllustration/portfolio2.png");
-            project4.setVisible(true);
+                projectRepository.save(project3);
 
-            projectRepository.save(project4);
+
+                Projects project4 = new Projects();
+                project4.setProjectName("Portfolio");
+                project4.setProjectDescription("A portfolio website that showcases my projects and skills.");
+                project4.setBackendStack("Java - Spring Boot");
+                project4.setFrontendStack("ReactJs - Vite");
+                project4.setDatabaseStack("SQLite");
+                project4.setBackendDeploymentStack("Heroku");
+                project4.setFrontendDeploymentStack("Netlify");
+                project4.setProjectLink("https://nilswenting.com/");
+                project4.setImageUrl1("/projectIllustration/portfolio1.png");
+                project4.setImageUrl2("/projectIllustration/portfolio2.png");
+                project4.setVisible(true);
+
+                projectRepository.save(project4);
+            } else {
+                System.out.println("Database already initialized");
+            }
+        } catch (Exception e) {
+
+
 
         }
 
@@ -108,5 +122,33 @@ public class DatabaseInitializer implements CommandLineRunner {
 
 
 
+
+
+
+    }
+
+
+    private void waitForDatabase() {
+        int maxAttempts = 10;
+        int attempt = 0;
+        boolean tablesCreated = false;
+
+        while (!tablesCreated && attempt < maxAttempts) {
+            try {
+                // Vérifie si la table existe
+                entityManager.createNativeQuery("SELECT 1 FROM projects LIMIT 1").getResultList();
+                tablesCreated = true;
+
+            } catch (Exception e) {
+                attempt++;
+
+                try {
+                    Thread.sleep(2000); // Attendre 2 secondes avant de réessayer
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
     }
 }
