a3e723a92044830d9176312a27f2b892fbc56f07
nilsw13 <nils.wenting@laplateforme.io>
2025-02-06 04:13:02 +0100

    set up sqlite config

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS projects (
                                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        project_name VARCHAR(255),
-    project_description TEXT,
-    backend_stack VARCHAR(255),
-    frontend_stack VARCHAR(255),
-    database_stack VARCHAR(255),
-    backend_deployment_stack VARCHAR(255),
-    frontend_deployment_stack VARCHAR(255),
-    project_link VARCHAR(255),
-    image_url_1 VARCHAR(255),
-    image_url_2 VARCHAR(255),
-    visible BOOLEAN
-    );
+                                        project_name TEXT NOT NULL,
+                                        project_description TEXT,
+                                        backend_stack TEXT,
+                                        frontend_stack TEXT,
+                                        database_stack TEXT,
+                                        backend_deployment_stack TEXT,
+                                        frontend_deployment_stack TEXT,
+                                        project_link TEXT,
+                                        image_url_1 TEXT,
+                                        image_url_2 TEXT,
+                                        visible INTEGER DEFAULT 0
+);
